1b02a37fbf6909cd4805d8a37b2dfcd3f9b12b07
Nolan Prochnau <parvus.mortalis@gmail.com>
2025-07-08 19:50:35 -0400

    Add video

--- src/data/media.ts
+++ src/data/media.ts
@@ -1,9 +1,13 @@
 export const clips = [
   {
+    title: 'Senior Recital',
+    youtube_id: 'QMHRi55OZHE',
+    description: `Recorded Spring 2025`
+  },
+  {
     title: 'Romanze for Viola and Orchestra',
     composer: 'Max Bruch',
     youtube_id: 'UIJ8JQGzyf4',
-    pin: false,
     description: `Recorded Spring 2022`
-  }
+  },
 ];
