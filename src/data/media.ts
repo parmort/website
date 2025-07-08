17e84972ed7241116b97fa348be775a75527de37
Nolan Prochnau <parvus.mortalis@gmail.com>
2025-07-08 17:02:32 -0400

    Add media page

---
+++ src/data/media.ts
@@ -0,0 +1,9 @@
+export const clips = [
+  {
+    title: 'Romanze for Viola and Orchestra',
+    composer: 'Max Bruch',
+    youtube_id: 'UIJ8JQGzyf4',
+    pin: false,
+    description: `Recorded Spring 2022`
+  }
+];
